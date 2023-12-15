9ca74c853846fcccaa41559bd4c139d9e7d3aa12
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-15 05:43:28 -0300

    case

--- LeetCode/exercise01/obj/Debug/net8.0/exercise01.AssemblyInfo.cs
+++ LeetCode/exercise01/obj/Debug/net8.0/exercise01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ec74c08dfc712788afbb62702f6efe85970418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0fe0835a38814ab559f07a08cf26e4814b9147")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
