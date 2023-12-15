76ee065c4734882b024b0332e221608b9bbf3a01
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-15 08:24:35 -0300

    case 3

--- LeetCode/exercise03/Program.cs
+++ LeetCode/exercise03/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution {
+    public string DestCity(IList<IList<string>> paths) {
+        HashSet<string> origem = new HashSet<string>();
+        HashSet<string> destino = new HashSet<string>();
+        foreach (var path in paths) {
+            origem.Add(path[0]);
+            destino.Add(path[1]);
+        }
+        foreach (var cidade in destino) {
+            if (!origem.Contains(cidade)) {
+                return cidade;
+            }
+        }
+        return "";
+    }
+static void Main(string[] args) {
+    
+}
+}
