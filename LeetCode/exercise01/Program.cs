3aa12815e83c5c87db5466fe19f8669174bdb7d5
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-15 06:13:19 -0300

    Solution

--- LeetCode/exercise01/Program.cs
+++ LeetCode/exercise01/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+
+    public class Solution
+    {
+        public int[] nums = [2, 7, 11, 5];
+        public int target = 9;
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int cur = nums[i];
+                int x = target - cur;
+
+                if (map.ContainsKey(x))
+                {
+                    return new int[] { map[x], i };
+                }
+                map[cur] = i;
+            }
+            return null;
+        }
+    }
+    static void Main(string[] args)
+    {
+
+    }
+}
--- LeetCode/exercise01/obj/Debug/net8.0/exercise01.AssemblyInfo.cs
+++ LeetCode/exercise01/obj/Debug/net8.0/exercise01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0fe0835a38814ab559f07a08cf26e4814b9147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca74c853846fcccaa41559bd4c139d9e7d3aa12")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
