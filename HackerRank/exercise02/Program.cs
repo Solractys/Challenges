82c6b381971e3da4ec1dd1c37b2f16506fb9d75d
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-16 14:25:23 -0300

    easyyyyyyyyyy

--- HackerRank/exercise02/Program.cs
+++ HackerRank/exercise02/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static List<int> reverseArray(List<int> a)
+    {
+    a.Reverse();
+    a.ToArray();
+    return a;
+    }  
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> res = Result.reverseArray(arr);
+
+        textWriter.WriteLine(String.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
