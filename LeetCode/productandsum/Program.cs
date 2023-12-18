14a655c69d89e0b5ca664c75b9535fbadb6b78a1
Edu <carloseduardosilvasouzadelima@gmail.com>
2023-12-18 13:36:19 -0300

    YES. I'M BETTER

--- LeetCode/productandsum/Program.cs
+++ LeetCode/productandsum/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4,2,5,9,7,4,8};
+            Array.Sort(nums);
+            int num = nums.Length;
+            int maximum = (nums[num - 1] * nums [num - 2]) - (nums[0] * nums[1]);
+
+            Console.WriteLine(maximum);
+        }
+    }
+}
